f2f0cfcd9fedb21745daaf5106c4998b69b7194f
Lukas Rascius <Lukas Rascius@localhost>
2015-03-20 18:48:27 -0400

    Adding ceasar_shift_encode

--- cipherfunctions.py
+++ cipherfunctions.py
@@ -1,5 +1,14 @@
+# Function that applies a ceasar shift on a piece of text. The encrypted text is returned. 
 def ceasar_shift_encode(text, shift):
-	pass
+	# List of 26 lowercase letters
+	alphabet = list(map(chr, range(97, 123)))
 
-def ceasar_shift_decode(text, shift):
-	pass
+	encoded_text = ""
+	text = "".join(text.lower().split())
+	for char in text:
+		if(char in alphabet):
+			char = alphabet[(alphabet.index(char) + shift) % len(alphabet)]
+			encoded_text += char
+	return encoded_text.upper()
+
+
