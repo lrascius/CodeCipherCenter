261412339c28d65c86c18651ea80547f395fc598
Marc Kurtz <mkurtz19@gmail.com>
2015-03-29 18:15:36 -0400

    created some templates for base page, main page, and challenge page.

--- cccenter/urls.py
+++ cccenter/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import patterns, url
 from cccenter import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name="index"),
+                       url(r'^$', views.index, name="cccenter"),
                        url(r'^getcipher/$', views.getCipher, name="getCipher"),
                       )
