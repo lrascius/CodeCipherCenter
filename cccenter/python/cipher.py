c7c31795a16ed9f7f71c20ce620edbbe9037d2fa
Marc Kurtz <mkurtz19@gmail.com>
2015-04-13 18:47:17 -0400

    create challenge function finished

--- cccenter/python/cipher.py
+++ cccenter/python/cipher.py
@@ -1,6 +1,10 @@
 #!cccenter/python/cipher.py
 '''Generates ciphertext objects.'''
 
+from cccenter.models import *
+from django.contrib.auth.models import User
+from django.utils import timezone
+
 def ceasar_shift_encode(text, shift):
     '''Function that applies a ceasar shift on a piece of text. The encrypted text is returned.'''
     # List of 26 lowercase letters
@@ -16,4 +20,25 @@ def ceasar_shift_encode(text, shift):
 
 def create_challenge(plaintext, ciphertext, ciphertype, key,
                      challenge_type, users = None, dt_created = None, dt_solved = None, solved_by = None):
-    pass
+            
+    if dt_created is not None:
+        dt_created = dt_created
+    else:
+        dt_created = timezone.now()
+                  
+    challenge = Challenge.objects.create(plaintext = plaintext, ciphertext = ciphertext, ciphertype = ciphertype,
+                                         cipherkey = key, challenge_type = challenge_type, datetime_created = dt_created)
+                                         
+    if users is not None:
+        for user in users:
+            challenge.users.add(user)
+    
+    if dt_solved is not None:
+        challenge.datetime_solved = dt_solved
+        
+    if solved_by is not None:
+        challenge.solved_by = solved_by
+        
+    challenge.save()
+    
+    return challenge.ciphertext, challenge.id
