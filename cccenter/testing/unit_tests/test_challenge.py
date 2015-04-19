5623acb83db816edf1c27e7320e470727a7e0149
Marc Kurtz <mkurtz19@gmail.com>
2015-04-19 17:12:57 -0400

    Test Fail1 written for cccenter.python.challenge.join_challenge. Tests for invalid challenge_id raises ValueError.

--- cccenter/testing/unit_tests/test_challenge.py
+++ cccenter/testing/unit_tests/test_challenge.py
@@ -15,7 +15,19 @@ class TestChallenge(TestCase):
     def test_joinChallenge_Pass1(self, mock_challenge, mock_models, mock_user):
         mock_models.Challenge.objects.get.return_value = mock_challenge
         mock_user.objects.get.return_value= "user"
+        mock_challenge.users.add.return_value = True
         
         success = join_challenge(challenge_id=1, user_id=2)
         
         mock_challenges.users.add.assert_called_with("user")
+        self.assertTrue(success)
+        
+    @mock.patch('cccenter.python.cipher.User')
+    @mock.patch('cccenter.python.cipher.models')
+    @mock.patch('cccenter.python.cipher.models.Challenge')
+    def test_joinChallenge_Fail1(self, mock_challenge, mock_models, mock_user):
+        mock_models.Challenge.objects.get.return_value = None
+        mock_user.objects.get.return_value= "user"
+        
+        with self.assertRaises(ValueError):
+            success = join_challenge(challenge_id=-1, user_id=2)
