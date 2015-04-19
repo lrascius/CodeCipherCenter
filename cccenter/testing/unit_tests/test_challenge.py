75412b895afa6ba138c9d67f5ddf56b3d5143a53
Marc Kurtz <mkurtz19@gmail.com>
2015-04-19 17:18:10 -0400

    Test Fail3 for cccenter.python.challenge.join_challenge. Tests for invalid type for challenge_id.

--- cccenter/testing/unit_tests/test_challenge.py
+++ cccenter/testing/unit_tests/test_challenge.py
@@ -41,3 +41,16 @@ class TestChallenge(TestCase):
         
         with self.assertRaises(ValueError):
             success = join_challenge(challenge_id=1, user_id=-2)
+            
+    @mock.patch('cccenter.python.cipher.User')
+    @mock.patch('cccenter.python.cipher.models')
+    @mock.patch('cccenter.python.cipher.models.Challenge')
+    def test_joinChallenge_Fail3(self, mock_challenge, mock_models, mock_user):
+        mock_models.Challenge.objects.get.return_value = None
+        mock_user.objects.get.return_value= "user"
+        
+        with self.assertRaises(TypeError):
+            success = join_challenge(challenge_id=1.0, user_id=-2)
+            
+        with self.assertRaises(TypeError):
+            success = join_challenge(challenge_id='1', user_id=-2)
