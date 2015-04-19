232efc48662933ac02fbf6f759349ece78fd94d9
Andrew Williams <williams.andrew15@gmail.com>
2015-04-19 15:49:20 -0400

    challenge.py

--- cccenter/python/challenge.py
+++ cccenter/python/challenge.py
@@ -1,9 +1,14 @@
 '''Generates challenge lists'''
 
-from cccenter.models import *
+import cccenter.models as models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
 def challenge_list():
+    chList = []
+    i = Challenge.objects.all()
 
-    
+    for names in i:
+        chList.append(names)
+
+
