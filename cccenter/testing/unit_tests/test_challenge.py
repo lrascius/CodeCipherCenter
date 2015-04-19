700e538b6ef5410ba092d0f824cbc5b5271239ad
Marc Kurtz <mkurtz19@gmail.com>
2015-04-19 17:14:41 -0400

    Test Fail2 for cccenter.python.challenge.join_challenge written. Tests for invalid user_id raising ValueError.

--- cccenter/testing/unit_tests/test_challenge.py
+++ cccenter/testing/unit_tests/test_challenge.py
@@ -31,3 +31,13 @@ class TestChallenge(TestCase):
         
         with self.assertRaises(ValueError):
             success = join_challenge(challenge_id=-1, user_id=2)
+            
+    @mock.patch('cccenter.python.cipher.User')
+    @mock.patch('cccenter.python.cipher.models')
+    @mock.patch('cccenter.python.cipher.models.Challenge')
+    def test_joinChallenge_Fail2(self, mock_challenge, mock_models, mock_user):
+        mock_models.Challenge.objects.get.return_value = None
+        mock_user.objects.get.return_value= "user"
+        
+        with self.assertRaises(ValueError):
+            success = join_challenge(challenge_id=1, user_id=-2)
