cfc466aa898cadf1204e999e69676b162f839b3d
Marc Kurtz <mkurtz19@gmail.com>
2015-04-19 22:49:59 -0400

    attempting to resolve csrf token issue with ajax header setting

--- cccenter/static/cccenter/challenge_page.js
+++ cccenter/static/cccenter/challenge_page.js
@@ -3,6 +3,18 @@
  * This page is linked to the challenge html pages to get ciphertexts, check plaintexts,
  * and run the comments and forum sections.
  */
+ 
+var csrftoken = $.cookie('csrftoken');
+function csrfSafeMethod(method) {
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 
 function getCiphertext() {
     // get a ciphertext
