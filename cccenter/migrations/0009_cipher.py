5521aad187b6e2787f0283606b81fd872b212401
Marc Kurtz <mkurtz19@gmail.com>
2015-04-26 12:08:29 -0400

    merge commit

--- cccenter/migrations/0009_cipher.py
+++ cccenter/migrations/0009_cipher.py
@@ -14,9 +14,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Cipher',
             fields=[
+<<<<<<< local
+                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+=======
                 ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+>>>>>>> other
                 ('ciphertype', models.CharField(max_length=200)),
+<<<<<<< local
+                ('difficulty', models.CharField(max_length=50, choices=[('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('advanced', 'Advanced')])),
+=======
                 ('difficulty', models.CharField(choices=[('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('advanced', 'Advanced')], max_length=50)),
+>>>>>>> other
             ],
             options={
             },
