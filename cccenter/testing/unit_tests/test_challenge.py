495bc699500b3b311dc541e7559821f996796ec7
Marc Kurtz <mkurtz19@gmail.com>
2015-04-25 22:50:14 -0400

    Fixed the tests for cccenter.python.challenge.user_in_challenge to return the a value for the mock_challenge object when checked for correct id.

--- cccenter/testing/unit_tests/test_challenge.py
+++ cccenter/testing/unit_tests/test_challenge.py
@@ -104,7 +104,8 @@ class TestChallenge(TestCase):
     @mock.patch('cccenter.python.challenge.models.Challenge')
     def test_userInChallenge_Pass1(self, mock_challenge, mock_models, mock_user):
         #mock_user.challenge_set.filter.return_value = mock_user
-        mock_models.Challenge.user_set.filter.return_value = mock_user
+        mock_models.Challenge.objects.get.return_value = mock_challenge
+        mock_challenge.user_set.filter.return_value = mock_user
         mock_user.exists.return_value = True
         mock_user.id = 2
         
@@ -119,7 +120,8 @@ class TestChallenge(TestCase):
     @mock.patch('cccenter.python.challenge.models.Challenge')
     def test_userInChallenge_Pass2(self, mock_challenge, mock_models, mock_user):
         #mock_user.challenge_set.filter.return_value = mock_user
-        mock_models.Challenge.user_set.filter.return_value = mock_user
+        mock_models.Challenge.objects.get.return_value = mock_challenge
+        mock_challenge.user_set.filter.return_value = mock_user
         mock_user.exists.return_value = False
         mock_user.id = 2
         
@@ -134,7 +136,8 @@ class TestChallenge(TestCase):
     @mock.patch('cccenter.python.challenge.models.Challenge')
     def test_userInChallenge_Fail1(self, mock_challenge, mock_models, mock_user):
         #mock_user.challenge_set.filter.return_value = mock_user
-        mock_models.Challenge.user_set.filter.return_value = mock_user
+        mock_models.Challenge.objects.get.return_value = mock_challenge
+        mock_challenge.user_set.filter.return_value = mock_user
         mock_user.exists.return_value = True
         mock_user.id = 2
         
@@ -149,7 +152,8 @@ class TestChallenge(TestCase):
     @mock.patch('cccenter.python.challenge.models.Challenge')
     def test_userInChallenge_Fail2(self, mock_challenge, mock_models, mock_user):
         #mock_user.challenge_set.filter.return_value = mock_user
-        mock_models.Challenge.user_set.filter.return_value = mock_user
+        mock_models.Challenge.objects.get.return_value = None
+        mock_challenge.user_set.filter.return_value = mock_user
         mock_user.exists.return_value = True
         mock_user.id = 2
         
