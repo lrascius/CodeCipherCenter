6e6cac75191ea0e5511c0aca600113cab350d4aa
Lukas Rascius <Lukas Rascius@localhost>
2015-04-27 13:03:27 -0400

    Adding the challenge list page

---
+++ cccenter/migrations/0009_cipher.py
@@ -0,0 +1,53 @@
+<<<<<<< local
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cccenter', '0008_merge'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Cipher',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('ciphertype', models.CharField(max_length=200)),
+                ('difficulty', models.CharField(max_length=50, choices=[(b'beginner', b'Beginner'), (b'intermediate', b'Intermediate'), (b'advanced', b'Advanced')])),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
+=======
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cccenter', '0008_auto_20150425_2254'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Cipher',
+            fields=[
+                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('ciphertype', models.CharField(max_length=200)),
+                ('difficulty', models.CharField(choices=[('beginner', 'Beginner'), ('intermediate', 'Intermediate'), ('advanced', 'Advanced')], max_length=50)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
+>>>>>>> other
---
+++ cccenter/models.py
@@ -0,0 +1,55 @@
+#!cccenter/models.py
+'''This file contains Django the models for the cccenter app.'''
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.encoding import smart_text
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    '''A model for storing users.'''
+    user = models.OneToOneField(User)
+    profile_image = models.ImageField(upload_to='images/profile',
+                                      default='images/profile/default.png')
+
+    def __unicode__(self):
+        return smart_text(self.user)
+
+class Challenge(models.Model):
+    '''A model for ciphertext display and plaintext submission.  Contains
+       fields for the ciphertext (display only) and the plantext.'''
+    ciphertext = models.TextField(default="")
+    plaintext = models.TextField(default="")
+    ciphertype = models.CharField(max_length=200)
+    cipherkey = models.TextField(default="")
+    users = models.ManyToManyField(User, related_name="user_challenge_set", blank=True, null=True)
+    datetime_created = models.DateTimeField()
+    datetime_solved = models.DateTimeField(blank=True, null=True)
+    solved_by = models.OneToOneField(User, blank=True, null=True)
+    challenge_type = models.CharField(max_length=50, choices=(('single', 'single'),
+                                                              ('collaborative', 'collaborative'),
+                                                              ('competitive', 'competitive')))
+    solved = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return smart_text(self.ciphertype)
+
+
+class Comment(models.Model):
+    '''A model for comments in the forums.'''
+    user = models.OneToOneField(User)
+    text = models.TextField(default="")
+    datetime = models.DateTimeField()
+    forum = models.ForeignKey(Challenge)
+
+    def __unicode__(self):
+        return smart_text(self.user)
+
+class Cipher(models.Model):
+    '''A model for ciphers including their difficulty and function to create one'''
+    ciphertype = models.CharField(max_length=200)
+    difficulty = models.CharField(max_length=50, choices=(('beginner', 'Beginner'),
+                                                          ('intermediate', 'Intermediate'),
+                                                          ('advanced', 'Advanced'))) 
+    def __unicode__(self):
+        return smart_text(self.ciphertype)
