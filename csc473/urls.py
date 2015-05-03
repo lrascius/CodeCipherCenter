3ef1c1bd4e840f58703c09c09f6ad26ca59e0f5f
Marc Kurtz <mkurtz19@gmail.com>
2015-05-03 13:43:07 -0400

    Another attempted fix for static asset delivery on Heroku by adding a static url to csc473/urls.py.

--- csc473/urls.py
+++ csc473/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from cccenter import views
+from django.conf import settings
 
 urlpatterns = patterns('',
-    # Examples:
+    # static assets
+    url(r'^static/(?P<document>)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT}),
+
     # url(r'^$', 'csc473.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.index, name="cccenter"),
