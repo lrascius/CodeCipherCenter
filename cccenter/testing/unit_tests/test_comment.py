0e892573a7dc5fb18fc9e5411d9302d8d308c8b4
Marc Kurtz <mkurtz19@gmail.com>
2015-05-04 23:26:50 -0400

    Wrote test Fail2 for cccenter.python.comment.get_comments. Tests for valueerror.

--- cccenter/testing/unit_tests/test_comment.py
+++ cccenter/testing/unit_tests/test_comment.py
@@ -27,3 +27,14 @@ class TestComment(TestCase):
             comments = get_comments(challenge_id=1.0)
         with self.assertRaises(TypeError):
             comments = get_comments(challenge_id='1')
+            
+    @mock.patch('cccenter.python.comment.Comment')
+    @mock.patch('cccenter.python.comment.Challenge')
+    @mock.patch('cccenter.python.comment.User')
+    @mock.patch('cccenter.python.comment.models')
+    def test_get_comments_Fail2(self, mock_comment, mock_challenge, mock_user, mock_models):
+        mock_models.Challenge.objects.get.return_value = mock_challenge
+        mock_challenge.challenge_comment_set.return_value = [mock_comment]
+        
+        with self.assertRaises(ValueError):
+            comments = get_comments(challenge_id=-1)
