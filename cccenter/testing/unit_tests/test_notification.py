2c442a145823084c2713164f169435ad70477ba4
Marc Kurtz <mkurtz19@gmail.com>
2015-05-05 18:19:12 -0400

    Fixed test test_notification_filters_by_user for cccenter.python.notification.get_notifications.

--- cccenter/testing/unit_tests/test_notification.py
+++ cccenter/testing/unit_tests/test_notification.py
@@ -1,14 +1,17 @@
 from django.test import TestCase
-from cccenter.models import *
+from cccenter.python.notification import *
 import mock
 
 class TestNotification(TestCase):
 
-    def test_notification_filters_by_user(self):
-        user = mock.Mock()
-        notification = mock.Mock(spec=models.Notification)
-        models.Notification.get_notifications(notification, user)
-        notification.filter.assert_called_with(user=user)        
+    @mock.patch('cccenter.python.notification.Notification')
+    def test_notification_filters_by_user(self, mock_notification):
+        mock_notification.objects.filter.return_value = mock_notification
+        
+        res = get_notifications('user')
+        
+        self.assertEqual(res, mock_notification)
+        mock_notification.objects.filter.assert_called_with(user='user')        
 
     def test_unviewed_notification(self):
         user = mock.Mock()
