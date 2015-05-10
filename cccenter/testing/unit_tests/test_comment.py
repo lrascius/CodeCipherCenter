aff28e903e7ac1690e1dc26be4f6f0752ba4dcb3
Marc Kurtz <mkurtz19@gmail.com>
2015-05-10 13:55:22 -0400

    Problems with tests for cccenter.python.comment fixed. Some mock objects could not find their references.

--- cccenter/testing/unit_tests/test_comment.py
+++ cccenter/testing/unit_tests/test_comment.py
@@ -3,11 +3,10 @@ from cccenter.python.comment import *
 import mock
 
 class TestComment(TestCase):
-    @mock.patch('cccenter.python.comment.Comment')
-    @mock.patch('cccenter.python.comment.Challenge')
-    @mock.patch('cccenter.python.comment.User')
+    @mock.patch('cccenter.python.comment.models.Challenge')
     @mock.patch('cccenter.python.comment.models')
-    def test_get_comments_Pass1(self, mock_comment, mock_challenge, mock_user, mock_models):
+    @mock.patch('cccenter.python.comment.models.Comment') #TODO change reference once comment is imported directly
+    def test_get_comments_Pass1(self, mock_comment, mock_models, mock_challenge):
         mock_models.Challenge.objects.get.return_value = mock_challenge
         mock_challenge.challenge_comment_set.all.return_value = [mock_comment]
         
@@ -15,11 +14,10 @@ class TestComment(TestCase):
         
         self.assertEqual(comments, [mock_comment])
         
-    @mock.patch('cccenter.python.comment.Comment')
-    @mock.patch('cccenter.python.comment.Challenge')
-    @mock.patch('cccenter.python.comment.User')
+    @mock.patch('cccenter.python.comment.models.Challenge')
     @mock.patch('cccenter.python.comment.models')
-    def test_get_comments_Fail1(self, mock_comment, mock_challenge, mock_user, mock_models):
+    @mock.patch('cccenter.python.comment.models.Comment') #TODO change reference once comment is imported directly
+    def test_get_comments_Fail1(self, mock_comment, mock_models, mock_challenge):
         mock_models.Challenge.objects.get.return_value = mock_challenge
         mock_challenge.challenge_comment_set.return_value = [mock_comment]
         
@@ -28,11 +26,10 @@ class TestComment(TestCase):
         with self.assertRaises(TypeError):
             comments = get_comments(challenge_id='1')
             
-    @mock.patch('cccenter.python.comment.Comment')
-    @mock.patch('cccenter.python.comment.Challenge')
-    @mock.patch('cccenter.python.comment.User')
+    @mock.patch('cccenter.python.comment.models.Challenge')
     @mock.patch('cccenter.python.comment.models')
-    def test_get_comments_Fail2(self, mock_comment, mock_challenge, mock_user, mock_models):
+    @mock.patch('cccenter.python.comment.models.Comment') #TODO change reference once comment is imported directly
+    def test_get_comments_Fail2(self, mock_comment, mock_models, mock_challenge):
         mock_models.Challenge.objects.get.return_value = None
         mock_challenge.challenge_comment_set.return_value = [mock_comment]
         
