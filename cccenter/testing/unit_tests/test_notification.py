2aa07de70f0f583cd33a3f921a80dc30abefc06e
Marc Kurtz <mkurtz19@gmail.com>
2015-05-10 13:36:14 -0400

    Wrote test Pass2 for cccenter.python.notification.get_notifications.

--- cccenter/testing/unit_tests/test_notification.py
+++ cccenter/testing/unit_tests/test_notification.py
@@ -5,14 +5,24 @@ import mock
 class TestNotification(TestCase):
 
     @mock.patch('cccenter.python.notification.Notification')
-    def test_notification_filters_by_user(self, mock_notification):
+    def test_notification_filters_by_user_Pass1(self, mock_notification):
         mock_notification.objects.filter.return_value = mock_notification
         mock_notification.order_by.return_value = [mock_notification]
         
         res = get_notifications('user')
         
         self.assertEqual(res, [mock_notification])
-        mock_notification.objects.filter.assert_called_with(user='user')        
+        mock_notification.objects.filter.assert_called_with(user='user')   
+        
+    @mock.patch('cccenter.python.notification.Notification')
+    def test_notification_filters_by_user_Pass2(self, mock_notification):
+        mock_notification.objects.filter.return_value = mock_notification
+        mock_notification.order_by.return_value = [mock_notification]
+        
+        res = get_notifications('user', get_all=True)
+        
+        self.assertEqual(res, [mock_notification])
+        mock_notification.objects.filter.assert_called_with(user='user')   
 
     @mock.patch('cccenter.python.notification.Notification')
     def test_unviewed_notification(self, mock_notification):
